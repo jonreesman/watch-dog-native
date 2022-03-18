14fc7fcbcc9cd3a812fd77dc5c47ced271cee2e8
Jreesman1 <Jreesman1@mac.com>
2022-03-18 06:35:31 -0500

    Initial commit

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, StyleSheet, View, TouchableOpacity } from 'react native';
+
+const HomeScreen = () => {
+
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default HomeScreen;
---
+++ src/screens/TickerScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, StyleSheet, View, TouchableOpacity } from 'react native';
+
+const TickerScreen = () => {
+
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default TickerScreen;
